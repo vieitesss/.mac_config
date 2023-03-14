b5789a38015f095cea1b6969eae01ff8f5d1c981
Daniel Vieites <dvieitest@gmail.com>
2023-03-14 09:34:52 +0100

    docker aliases

--- aliases/docker.aliases.sh
+++ aliases/docker.aliases.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Docker-compose ls -a (running files)
 dockerComposeDown () {
  cd $(docker-compose ls -a | tail -n +2 | awk '{print $3}' | fzf | sed 's#/[^/]*$##') && docker-compose down -t 0
 }
 alias dcd=dockerComposeDown
 
 alias dcu='docker-compose up -d'
+alias dps='docker ps'
