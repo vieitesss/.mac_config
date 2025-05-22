ce7e27bc6a7cb45e8a0e5150628798bf9289a7a7
Daniel Vieites <dvieitest@gmail.com>
2025-05-22 22:47:07 +0200

    bar config

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -5,21 +5,6 @@ then
 	FOCUSED=$(aerospace list-workspaces --focused)
 fi
 
-# not_empty=$(aerospace list-workspaces --monitor 1 --empty no)
-#
-# CURRENT=""
-# NOT_EMPTY=""
-# EMPTY=""
-
-# if [[ "aerospace.$current" == "$NAME" ]]
-# then
-# 	sketchybar --set "$NAME" icon="$FOCUSED" icon.color=0xffcba6f7
-# elif [[ "$not_empty" =~ "$number" ]]
-# then
-# 	sketchybar --set "$NAME" icon="$NOT_EMPTY" icon.color=0xffcba6f7
-# else
-# 	sketchybar --set "$NAME" icon="$EMPTY" icon.color=0xffcdd6f4
-# fi
 color="0xffcdd6f4"
 
 default=(
@@ -42,7 +27,10 @@ focused=(
 
 if [[ "aerospace.$FOCUSED" == "$NAME" ]]
 then
-	sketchybar --set "$NAME" "${focused[@]}"
-else
-	sketchybar --set "$NAME" "${default[@]}"
+	sketchybar --animate linear 5 \
+		   --set "$NAME" "${focused[@]}"
+elif [[ "aerospace.$PREV" == "$NAME" ]]
+then
+	sketchybar --animate linear 5 \
+		   --set "$NAME" "${default[@]}"
 fi
