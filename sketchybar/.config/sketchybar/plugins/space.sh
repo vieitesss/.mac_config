f8b4c9b0cc44225c6bc7494e906a6dc61886bdca
Daniel Vieites <dvieitest@gmail.com>
2025-08-22 00:26:05 +0200

    update sketchybar

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 
-if [[ -z "$FOCUSED" ]]
-then
-    FOCUSED=$(aerospace list-workspaces --focused)
-fi
+# if [[ -z "$FOCUSED" ]]
+# then
+#     FOCUSED=$(aerospace list-workspaces --focused)
+# fi
+#
+# default=(
+#     icon.background.height=10
+#     icon.background.corner_radius=10
+#     icon.padding_right=5
+#     icon.padding_left=5
+# )
+#
+# focused=(
+#     icon.background.drawing=true
+#     icon.background.height=16
+#     icon.background.corner_radius=10
+#     icon.padding_right=20
+#     icon.padding_left=20
+# )
+#
+# if [[ "aerospace.$FOCUSED" == "$NAME" ]]
+# then
+#     sketchybar --animate linear 5 \
+#                --set "$NAME" "${focused[@]}"
+# elif [[ "aerospace.$PREV" == "$NAME" ]]
+# then
+#     sketchybar --animate linear 5 \
+#                --set "$NAME" "${default[@]}"
+# fi
+#
+#
 
-default=(
-    icon.background.height=10
-    icon.background.corner_radius=10
-    icon.padding_right=5
-    icon.padding_left=5
-)
-
-focused=(
-    icon.background.drawing=true
-    icon.background.height=16
-    icon.background.corner_radius=10
-    icon.padding_right=20
-    icon.padding_left=20
-)
-
-if [[ "aerospace.$FOCUSED" == "$NAME" ]]
-then
-    sketchybar --animate linear 5 \
-               --set "$NAME" "${focused[@]}"
-elif [[ "aerospace.$PREV" == "$NAME" ]]
-then
-    sketchybar --animate linear 5 \
-               --set "$NAME" "${default[@]}"
-fi
+sketchybar --set aerospace label="$(aerospace list-workspaces --focused)"
